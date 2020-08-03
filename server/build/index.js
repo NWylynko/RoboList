64b3be4f88e37a1ff8577ed1789bb7567ba24968
Nick Wylynko <nick1014375@gmail.com>
2020-08-03 19:03:23 +0800

    process data and hold in memory

--- server/build/index.js
+++ server/build/index.js
@@ -5,10 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("source-map-support/register");
 var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = express_1.default();
 var port = 8080;
+app.use(body_parser_1.default.json());
+var data = {};
 app.get("/", function (req, res) {
-    res.send('yo');
+    res.send(data);
+});
+app.post("/", function (req, res) {
+    console.log(req.body);
+    var _a = req.body, id = _a.id, ip = _a.ip;
+    data[id] = { ip: ip };
+    res.send('success');
 });
 app.listen(port, function () {
     console.log("listening at http://localhost:" + port);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,22 @@
 import 'source-map-support/register'
 import express from "express";
+import bodyParser from 'body-parser';
 const app = express();
 const port = 8080;
 
+app.use(bodyParser.json())
+
+let data: any = {}
+
 app.get("/", (req, res) => {
-  res.send('yo')
+  res.send(data)
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body)
+  const { id, ip } = req.body;
+  data[id] = {ip}
+  res.send('success')
 });
 
 app.listen(port, () => {
