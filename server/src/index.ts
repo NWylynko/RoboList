b88cf36bf4cc1dc61151e96805388e31996504fa
Nick Wylynko <nick1014375@gmail.com>
2020-08-03 20:44:02 +0800

    add cors, setup validation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,17 @@
 import 'source-map-support/register'
 import express from "express";
 import bodyParser from 'body-parser';
+import cors from 'cors'
+import Joi from 'joi';
 const app = express();
 const port = 8080;
 
+const schema = Joi.object({
+  id: Joi.string().required(),
+  ip: Joi.string().required()
+})
+
+app.use(cors())
 app.use(bodyParser.json())
 
 let data: any = {}
@@ -12,11 +20,17 @@ app.get("/", (req, res) => {
   res.send(data)
 });
 
-app.post("/", (req, res) => {
+app.post("/",(req, res) => {
   console.log(req.body)
-  const { id, ip } = req.body;
-  data[id] = {ip}
-  res.send('success')
+  try {
+    // const { id, ip } : Joi.ValidationResult<any> = schema.validate(req.body);
+    const { id, ip } = req.body;
+    data[id] = {id, ip}
+    res.send('success')
+  } catch (error) {
+    res.send(error)
+  }
+  
 });
 
 app.listen(port, () => {
