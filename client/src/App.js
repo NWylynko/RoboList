53d354068769bc324f918cf8813ca4d632a2cc3d
Nick Wylynko <nick1014375@gmail.com>
2020-08-03 21:32:17 +0800

    move from fetch over to websocket for live updats

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
+let server = `ws://${window.location.hostname}:8080`
+let ws = new WebSocket(server)
+
 function App() {
 
   const [list, setList] = useState()
+  const [connected, setConnected] = useState(false)
 
   useEffect(() => {
-    fetch(`http://${window.location.hostname}:8080`)
-      .then(response => response.json())
-      .then(data => {
-        return Object.keys(data).map(id => {
-          return {id, ip: data[id].ip}
-        })
-      })
-      .then(setList)
-      .catch(console.warn);
+    ws.onopen = () => setConnected(true);
+    ws.onclose = () => setConnected(false);
+    ws.onmessage = ({ data }) => {
+      data = JSON.parse(data)
+      setList(Object.keys(data).map(id => {
+        return { id, ip: data[id].ip }
+      }))
+    }
   }, [])
 
   return (
     <div>
-      <h2>Robots</h2>
+      <h2>Robots {connected ? ' - connected to server' : ' - not connected to server'}</h2>
       {list ? list.map(Robot) : <p>loading...</p>}
     </div>
   );
 }
 
-function Robot({id, ip}) {
+function Robot({ id, ip }) {
   return (
-    <div key={id} style={{display: 'inline-flex', flexDirection: 'row'}}>
+    <div key={id} style={{ display: 'flex', flexDirection: 'row' }}>
       <span role="img" aria-label="robot">🤖</span>
       <p>local ip:</p>
       <a href={'ssh://pi@' + ip}>{ip}</a>
