b107300dae7c1bb4e0be6265703e2d266b7321ba
Nick Wylynko <nick1014375@gmail.com>
2020-08-04 21:21:40 +0800

    add message for no robots listed yet

--- client/src/App.js
+++ client/src/App.js
@@ -21,8 +21,13 @@ function App() {
 
   return (
     <div>
-      <h2 style={{paddingLeft: 15}}>Robots {connected ? ' - connected to server' : ' - not connected to server'}</h2>
-      {list ? list.map(Robot) : <p style={{color: 'var(--accent1)'}}>loading...</p>}
+      <h2 style={{ paddingLeft: 15 }}>Robots {connected ? ' - connected to server' : ' - not connected to server'}</h2>
+      {list ? 
+        list.length === 0 ? 
+          <p style={{ color: 'var(--accent1)' }}>No robots registered yet</p> : 
+          list.map(Robot) : 
+        <p style={{ color: 'var(--accent1)' }}>loading...</p>
+      }
     </div>
   );
 }
@@ -31,7 +36,7 @@ function Robot({ id, ip, hostname }) {
   return (
     <div key={id} style={{ display: 'flex', flexDirection: 'row', border: '2px solid var(--accent1)', borderRadius: 5, padding: 5, margin: 5, width: 'fit-content' }}>
       <p>Host: {hostname}</p>
-      <p>IP: <a href={'ssh://pi@' + ip} style={{margin: 0, padding: 0, color: 'var(--accent2)'}}>{ip}</a></p>
+      <p>IP: <a href={'ssh://pi@' + ip} style={{ margin: 0, padding: 0, color: 'var(--accent2)' }}>{ip}</a></p>
       <p>MAC: {id.toUpperCase()}</p>
     </div>
   )
