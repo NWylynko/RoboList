3851f8f20b18d3fe30c268ce73d75fbfd4e394a1
Nick Wylynko <nick1014375@gmail.com>
2020-08-04 10:39:29 +0800

    add hostname to register script

--- robot/register.sh
+++ robot/register.sh
@@ -1,13 +1,19 @@
 SERVER="http://192.168.0.109:8080"
 
+if [$(ifconfig wlan0) == '']
+then
+	exit;
+fi
+
 ID=$(ifconfig wlan0 | grep ether | awk '{ print $2 }')
 IP=$(ifconfig wlan0 | grep "inet " | awk '{ print $2 }')
 
-echo $SERVER
-echo $ID
-echo $IP
+echo Server: $SERVER
+echo Mac: $ID
+echo local ip: $IP
+echo hostname: $HOSTNAME
 
-BODY='{"id":"'$ID'","ip":"'$IP'"}'
+BODY='{"id":"'$ID'","ip":"'$IP'","hostname":"'$HOSTNAME'"}'
 echo $BODY
 
 curl -X POST $SERVER -d $BODY -H "Content-Type: application/json"
