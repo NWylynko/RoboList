9168d34248bf0c034bc499cfec2a7ce9b8001c5d
Nick Wylynko <nick1014375@gmail.com>
2020-08-10 18:04:44 +0800

    add pm2 file

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    name: "RoboListServer",
+    script: "./server/build/index.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }, {
+    name: "RoboListWebApp",
+    script: "serve",
+    env: {
+      PM2_SERVE_PATH: './client/build',
+      PM2_SERVE_PORT: 80,
+      PM2_SERVE_SPA: 'true',
+      PM2_SERVE_HOMEPAGE: './client/build/index.html'
+    }
+  }]
+}
