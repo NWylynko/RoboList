12d1f4f6f7ce2de882d88ebffa1b152b6b2647e4
Nick Wylynko <nick1014375@gmail.com>
2020-08-10 19:57:40 +0800

    change ifconfig to /sbin/ifconfig

--- robot/register.sh
+++ robot/register.sh
@@ -1,5 +1,7 @@
 SERVER="http://roboserver.local:8080"
 
+# /sbin/ifconfig needs to be used when the script is run through crontab for whatever reason
+
 # checks to see if wlan0 network interface is present
 # raspberrypis use wlan0 for the wifi but other devices
 # may use different names like wifi0
@@ -8,7 +10,7 @@ SERVER="http://roboserver.local:8080"
 # so if other ones need to be used
 # eg. wifi0, lan0, en0
 # change all the wlan0 in the script - not just this one
-if [$(ifconfig | grep wlan0) == '']
+if [$(/sbin/ifconfig | grep wlan0) == '']
 then
 	echo wlan0 interface not found
 	exit;
@@ -24,9 +26,9 @@ fi
 # if for some reason the mac address needs to be changed
 # or some kind of connection is made using both wifi and bluetooth
 # some other kind of unique identifier needs to be used
-ID=$(ifconfig wlan0 | grep ether | awk '{ print $2 }')
+ID=$(/sbin/ifconfig wlan0 | grep ether | awk '{ print $2 }')
 # use "inet " instead of just "inet" to remove any inet6 because we just want the ipv4
-IP=$(ifconfig wlan0 | grep "inet " | awk '{ print $2 }')
+IP=$(/sbin/ifconfig wlan0 | grep "inet " | awk '{ print $2 }')
 
 echo Server: $SERVER
 echo Mac: $ID
