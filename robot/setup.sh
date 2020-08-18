e4c4e4cff5bf58824911906de4b44eed092e593f
Nick Wylynko <nick1014375@gmail.com>
2020-08-18 19:56:17 +0800

    Update setup.sh

--- robot/setup.sh
+++ robot/setup.sh
@@ -1,7 +1,7 @@
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "* * * * * /home/pi/RoboList/robot/register.sh > /home/pi/RoboList/robot/register.log" >> mycron
+echo "* * * * * $(pwd)/register.sh > $(pwd)/register.log" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
